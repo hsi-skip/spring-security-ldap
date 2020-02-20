de94b07767d3bafa079463e6f072aaa108a18ccb
hassan <hsi.skip@gmail.com>
2020-02-20 00:22:38 +0000

    First commit

---
+++ src/main/java/com/hsi/springsecurityldap/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.hsi.springsecurityldap.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to spring security with ldap !";
+    }
+	
+}
---
+++ src/test/java/com/hsi/ldapapp/springsecurityldapApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hsi.ldapapp;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class springsecurityldapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
